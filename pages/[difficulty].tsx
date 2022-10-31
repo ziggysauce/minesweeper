59a0402078963e8c9dd7bffc718ac5d9569a0bed
ziggysauce <dtn3423@gmail.com>
2022-10-30 21:29:39 -0400

    touchevent

--- pages/[difficulty].tsx
+++ pages/[difficulty].tsx
@@ -105,7 +105,7 @@ const GameBoard = () => {
   // Prevent default behavior with right click
   // TODO: Only prevent right click on board
   if (typeof window !== 'undefined') {
-    window.addEventListener('contextmenu', (e?: MouseEvent) => {
+    window.addEventListener('contextmenu', (e: MouseEvent | TouchEvent) => {
       e.preventDefault();
     });
   }
