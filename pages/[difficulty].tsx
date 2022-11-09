200437c427a07c1c3084c2ce074853d11d3e6349
ziggysauce <dtn3423@gmail.com>
2022-11-09 00:01:49 -0500

    timer ref

--- pages/[difficulty].tsx
+++ pages/[difficulty].tsx
@@ -165,7 +165,7 @@ const GameBoard = () => {
 
   // FIXME: Timer logic
   if(!interval) {
-      const intervalId: string | number | Timer | null = setInterval(() => {
+      const intervalId: string | number | useRef<Timer>| null = setInterval(() => {
         setBoardTime(timer + 1);
       }, 1000);
       setTimerInterval(intervalId);
