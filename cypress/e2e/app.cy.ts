a86a443048d5942e1133eef71918c992d148c725
ziggysauce <dtn3423@gmail.com>
2022-12-24 16:59:24 -0500

    update test language

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,8 +3,8 @@ describe('Navigation', () => {
     // Start from the index page
     cy.visit('/');
 
-    // The new page should contain an h1 with 'Welcome to Minesweeper'
-    cy.get('h1').contains('Welcome to Minesweeper');
+    // The new page should contain an h1 with 'Welcome to Chem Cleanup'
+    cy.get('h1').contains('Welcome to Chem Cleanup');
   });
 });
 
