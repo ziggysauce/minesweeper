19fdade004e410eaafef558e24997a8fbc6fba56
ziggysauce <dtn3423@gmail.com>
2022-12-25 22:18:48 -0500

    more ui fixes

--- pages/[difficulty].tsx
+++ pages/[difficulty].tsx
@@ -316,7 +316,7 @@ function GameBoard() {
             Back
           </button>
         </Link>
-        <h1 className={classNames({'my-3 text-2xl': true}, gameHasWon ? 'text-blue-500' : 'text-transparent')}>Congrats! You won!</h1>
+        <h1 className={classNames({'my-3 text-2xl': true}, {'text-transparent': !gameHasWon})}>Congrats! You won!</h1>
         <div className="flex flex-col justify-center items-center border-8 border-gray-300 bg-gray-300">
           <div className="flex justify-between items-center w-full border-8 border-t-gray-500 border-l-gray-500 border-b-gray-100 border-r-gray-100 mb-2">
             <div className="mx-2 grow p-1">
